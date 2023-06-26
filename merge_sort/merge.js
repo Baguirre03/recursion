c57b3ed2c94ca42787ec0a0b7889520468bf2000
Ben Aguirre <ben.aguirre03@gmail.com>
2023-06-26 15:23:47 -0500

    sorts, doesnt merge on second step

--- merge_sort/merge.js
+++ merge_sort/merge.js
@@ -2,21 +2,21 @@
 // and returns a sorted array, using a recursive merge sort methodology.
 
 function mergeSort(array) {
-    console.log('passing through:', array)
+    // console.log('passing through:', array)
     if (array.length > 1) {
         let mid = (array.length) / 2
         let low = array.slice(0, mid)
         let high = array.slice(mid, array.length)
-        console.log('low: ', low)
-        console.log('high: ', high)
+        // console.log('low: ', low)
+        // console.log('high: ', high)
         mergeSort(low) 
         mergeSort(high)
         return merge(low, high)
     }
 }
 
-let c = []
 function merge(lowArray, highArray) {
+    let c = []
     console.log('merging:', lowArray, highArray)
     let i = 0; let j = 0; let k = 0;
     while (i < lowArray.length && j < highArray.length) {
@@ -33,10 +33,11 @@ function merge(lowArray, highArray) {
         c[k++] = highArray[j]
     }
     console.log('result: ', c)
-    return `final result = ${[c]}`
+    return c
 }
 
 // console.log(mergeSort([2]))
 // console.log(mergeSort([4, 2]))
 console.log(mergeSort([4,2,7,6]))
+// console.log(mergeSort([6,4,3,8,1,2]))
 // console.log(mergeSort([5, 3, 6, 0, 1, 11, 8, 3]))
